f4e1f43d55fb47d234c3080975364bc13f4f35d0
satya2544 <koppolu.satya@gmail.com>
2026-04-24 15:34:08 +0100

    session-06

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,13 +4,16 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
      echo "ERROR:: Please run this script with root privelege"
+     exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
      echo "ERROR:: Installing MySQL is failure"
+     exit 1
 else
      echo "Installing MySQL is SUCCESS"
+     
 fi          
      
