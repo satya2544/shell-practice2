683a119460f75949e59a307599f26923f66918b3
satya2544 <koppolu.satya@gmail.com>
2026-04-26 14:17:21 +0100

    session-06

---
+++ 13-colors.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1
+fi
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+       echo -e  "Installing $2 ... $R is failure $N"
+       exit 1
+    else
+       echo -e "Installing $2 ... $G is SUCCESS $N"
+     fi
+
+ }
+
+ dnf list installed mysql
+
+ if [ $? -ne 0 ]; then
+      dnf install mysql -y
+      VALIDATE $? "MYSQL"   
+ else
+    echo "MySQL already exist ... $Y SKIPPING $N"
+ fi           
+
+
+ dnf list installed nginx
+
+ if [ $? -ne 0 ]; then
+      dnf install nginx -y
+      VALIDATE $? "Nginx"   
+ else
+    echo "Nginx already exist ... $Y SKIPPING $N"
+ fi 
+
+dnf list installed python3
+
+ if [ $? -ne 0 ]; then
+      dnf install python3 -y
+      VALIDATE $? "Python3"   
+ else
+    echo "Python already exist ... $Y SKIPPING $N"
+ fi 
+
